78cb89f4ef73969915d45a791abb768a995eb644
Aimee <C22424534@mytudublin.ie>
2024-03-04 18:09:40 +0000

    setting up the repo

---
+++ java/src/ie/tudublin/Main.java
@@ -0,0 +1,27 @@
+
+// Packages must match the folder structure
+package ie.tudublin;
+
+public class Main
+{
+
+	public void helloProcessing()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio1());
+    }
+
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+		
+		
+		Main m = new Main();
+
+	
+
+		m.helloProcessing();
+	}
+	
+}
