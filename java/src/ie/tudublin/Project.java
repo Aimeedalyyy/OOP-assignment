7c6c2da41c6d0edeb7d78b5a2907e118be73ed87
Aimee <C22424534@mytudublin.ie>
2024-04-20 18:43:36 +0100

    making drawings react to sound

--- java/src/ie/tudublin/Project.java
+++ java/src/ie/tudublin/Project.java
@@ -11,12 +11,20 @@ public class Project extends PApplet
 {
     Minim minim;
     AudioPlayer banjo , boathorn , drums , guitar , whistle;
-    AudioBuffer SharedBuffer;
+    AudioBuffer SharedBuffer, drumBuffer;
     
     int mode = 0;
 
     //Cube Variables
     int OFF_MAX = 300;
+
+    //case 3 variables
+    float angle = 0;
+    float rotationSpeed = 0.01f; // Reduced rotation speed for the larger sphere
+    float sphereSpeed = 0.005f; // Reduced movement speed for the smaller spheres
+    float sphereSize = 10; // Size of individual spheres in the disco ball
+    float radius = 100; // Radius of the larger sphere
+    int numSpheres = 200; // Number of smaller spheres
 
   
     float[] lerpedBuffer;
@@ -58,6 +66,7 @@ public class Project extends PApplet
 
         //shared audio buffers
         SharedBuffer = banjo.mix;
+        drumBuffer = drums.mix;
 
         colorMode(HSB);
 
@@ -105,21 +114,131 @@ public class Project extends PApplet
                     }
                     }
                 }
-        
-
+            break;
             
-         
-                break;
-            
-            case 2:
+            case 2://lines 
+                stroke(255);
+                
+                for(int i = 0 ; i < drumBuffer.size() ; i ++)
+                {
 
-                break;
+                
+                float x = random(width);
+                float y = random(height);
+                float x2 = random(width);
+                float y2 = random(height);
+                line(x, y, x2, y2);
+    
+                //adding colour to the lines
+                stroke(random(255), random(255), random(255));
+                line(x, y, x2, y2);
+    
+                //adding thickness to the lines
+                strokeWeight(random(8));
+                line(x, y, x2, y2);
+    
+    
+                //adding transparency to the lines
+                stroke(random(255), random(255), random(255), random(255));
+                line(x, y, x2, y2);
+    
+                //adding lerping to the lines
+                float lerp = map(mouseX, 0, width, 0, 1);
+                float lerpedX = lerp(x, x2, lerp);
+                float lerpedY = lerp(y, y2, lerp);
+                line(x, y, lerpedX, lerpedY);
+            }
+
+
+            break;
+            case 3://spheres going around
+
+            noStroke(); // No stroke for the spheres
+            
+            background(0); // Clear the background
+
+            // Translate to the center of the canvas
+            translate(width / 2, height / 2);
+
+            // Rotate the scene around its center (larger sphere)
+            rotateY(angle);
+
+            // Draw the spinning disco ball
+            drawDiscoBall();
+
+
+            break;
+            case 4://mandala 
+            translate(width / 2, height / 2); 
         
-            default:
+            for (int i = 0; i < 12; i++) {
+                float fx = cos(angle) * 200;
+                float fy = sin(angle) * 200;
+    
+                stroke(random(255), random(255), random(255));
+    
+                ellipse(fx, fy, 400, 400); 
+                rotate(radians(30)); 
+            }
+            
+            angle += 0.05; 
+        
+            break;
+            case 5://sphere in cube 
+                drawCubeSphere(0.5f); // You can pass a default value here
+
+            break;
+        }
+            
+
+        
+        
     }
         
         
+    
+
+    void drawDiscoBall() {
+        // Loop through the number of smaller spheres
+        for (int i = 0; i < numSpheres; i++) {
+            // Calculate the position of the smaller sphere on the surface of the larger sphere
+            float theta = map(i, 0, numSpheres, 0, TWO_PI); // Evenly distribute spheres along the equator
+            float phi = map(i % (numSpheres / 2), 0, numSpheres / 2, -HALF_PI, HALF_PI); // Distribute spheres from top to bottom
+            float x = radius * sin(phi) * cos(theta);
+            float y = radius * sin(phi) * sin(theta);
+            float z = radius * cos(phi);
+
+            // Apply some variation in color
+            int color = color(random(255), random(255), random(255));
+
+            // Set fill color for the smaller sphere
+            fill(color);
+
+            // Draw the smaller sphere at the calculated position
+            pushMatrix();
+            translate(x, y, z);
+            sphere(sphereSize);
+            popMatrix();
+        }
     }
-            
+          
+    public void drawCubeSphere(float smoothedAmplitude) {
+        background(0);
+        lights(); 
+        float sphereSize = smoothedAmplitude * 200; 
+        float cubeSize = sphereSize * 2; 
+        
+        // Draw the cube
+        stroke(255); 
+        noFill();
+        translate(width / 2, height / 2, -cubeSize / 2); 
+        box(cubeSize); // Draw a cube
+        
+        // Draw the sphere inside the cube
+        noStroke();
+        fill(smoothedAmplitude * 255, 255, 255); 
+        translate(0, 0, cubeSize / 2 - sphereSize / 2); 
+        sphere(sphereSize); 
+    }
 
 }        
